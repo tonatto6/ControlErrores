a705e3aa720d48cfca1fb5fa252c95477c90e53c
ltonattosistemas@gmail.com <ltonattosistemas@gmail.com>
2022-06-16 17:36:34 -0300

    Agregacion del proyecto

---
+++ ControlDeErrores/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using ControlDeErrores.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ControlDeErrores.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    
+        public IActionResult NuevaPersona()
+        {
+            return View();
+        }
+    
+        [HttpPost]
+        public IActionResult NuevaPersona(Persona oPersona)
+        {
+            if(!ModelState.IsValid)
+            { return View(); }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ControlDeErrores/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlDeErrores.Models
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+        [MinLength(2, ErrorMessage = "El nombre debe ser mayor a 2 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El apellido no puede tener más de 100 caracteres")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MaxLength(8, ErrorMessage = "El DNI de la persona debe ser como máximo de 8 caracteres")]
+        [MinLength(8, ErrorMessage = "El DNI de la persona debe ser como mínimo de 8 caracteres")]
+        public string DNI { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria")]
+        [Range(18,60, ErrorMessage = "La edad debe estar comprendida entre 18 y 60 años")]
+        public int Edad { get; set; }
+    }
+}
